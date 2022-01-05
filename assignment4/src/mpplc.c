258e51f24e14e188cee2d7a96dde76e62d9e8732
shin <shin@localhost>
2022-01-05 00:44:02 +0000

    算術命令を追加

--- assignment4/src/mpplc.c
+++ assignment4/src/mpplc.c
@@ -14,14 +14,15 @@ void end_output(void){
 	fclose(outfp);
 }
 
-void LD(char *a, char *b){
-	fprintf(outfp,"\tLD\t%s, %s\n",a,b);
-}
+void LD(char *a, char *b){fprintf(outfp,"\tLD\t%s, %s\n",a,b);}
+void ST(char *a, char *b){fprintf(outfp,"\tST\t%s, %s\n",a,b);}
+void LAD(char *a, char *b){fprintf(outfp,"\tLAD\t%s, %s\n",a,b);}
 
-void ST(char *a, char *b){
-	fprintf(outfp,"\tST\t%s, %s\n",a,b);
-}
-
-void LAD(char *a, char *b){
-	fprintf(outfp,"\tLAD\t%s, %s\n",a,b);
-}
+void ADDA(char *a, char *b){fprintf(outfp,"\tADDA\t%s, %s\n",a,b);}
+void ADDL(char *a, char *b){fprintf(outfp,"\tADDL\t%s, %s\n",a,b);}
+void SUBA(char *a, char *b){fprintf(outfp,"\tSUBA\t%s, %s\n",a,b);}
+void SUBL(char *a, char *b){fprintf(outfp,"\tSUBL\t%s, %s\n",a,b);}
+void MULA(char *a, char *b){fprintf(outfp,"\tMULA\t%s, %s\n",a,b);}
+void MULL(char *a, char *b){fprintf(outfp,"\tMULL\t%s, %s\n",a,b);}
+void DIVA(char *a, char *b){fprintf(outfp,"\tDIVA\t%s, %s\n",a,b);}
+void DIVL(char *a, char *b){fprintf(outfp,"\tDIVA\t%s, %s\n",a,b);}
--- assignment4/src/mpplc.h
+++ assignment4/src/mpplc.h
@@ -6,3 +6,12 @@ void end_output(void);
 void LD(char *a, char *b);
 void ST(char *a, char *b);
 void LAD(char *a, char *b);
+
+void ADDA(char *a, char *b);
+void ADDL(char *a, char *b);
+void SUBA(char *a, char *b);
+void SUBL(char *a, char *b);
+void MULA(char *a, char *b);
+void MULL(char *a, char *b);
+void DIVA(char *a, char *b);
+void DIVL(char *a, char *b);
