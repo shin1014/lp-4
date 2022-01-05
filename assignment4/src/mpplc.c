340e20cbbeba929e42c8212347c22e05321d4a09
shin <shin@localhost>
2022-01-05 01:02:26 +0000

    比較演算命令を追加＆実行アドレス用の関数を追加

--- assignment4/src/mpplc.c
+++ assignment4/src/mpplc.c
@@ -15,6 +15,10 @@ void end_output(void){
 }
 
 void LD(char *a, char *b){fprintf(outfp,"\tLD\t%s, %s\n",a,b);}
+void LD_ra(char *a, char *b, char *x){
+	if(x==NULL) fprintf(outfp,"\tLD\t%s, %s\n",a,b);
+	else fprintf(outfp,"\tLD\t%s, %s, %s\n",a,b,x);
+}
 void ST(char *a, char *b){fprintf(outfp,"\tST\t%s, %s\n",a,b);}
 void LAD(char *a, char *b){fprintf(outfp,"\tLAD\t%s, %s\n",a,b);}
 
@@ -27,7 +31,27 @@ void MULL(char *a, char *b){fprintf(outfp,"\tMULL\t%s, %s\n",a,b);}
 void DIVA(char *a, char *b){fprintf(outfp,"\tDIVA\t%s, %s\n",a,b);}
 void DIVL(char *a, char *b){fprintf(outfp,"\tDIVL\t%s, %s\n",a,b);}
 
+void ADDA_ra(char *a, char *b, char *x){if(x==NULL) fprintf(outfp,"\tADDA\t%s, %s\n",a,b);else fprintf(outfp,"\tADDA\t%s, %s, %s\n",a,b,x);}
+void ADDL_ra(char *a, char *b, char *x){if(x==NULL) fprintf(outfp,"\tADDL\t%s, %s\n",a,b);else fprintf(outfp,"\tADDL\t%s, %s, %s\n",a,b,x);}
+void SUBA_ra(char *a, char *b, char *x){if(x==NULL) fprintf(outfp,"\tSUBA\t%s, %s\n",a,b);else fprintf(outfp,"\tSUBA\t%s, %s, %s\n",a,b,x);}
+void SUBL_ra(char *a, char *b, char *x){if(x==NULL) fprintf(outfp,"\tSUBL\t%s, %s\n",a,b);else fprintf(outfp,"\tSUBL\t%s, %s, %s\n",a,b,x);}
+void MULA_ra(char *a, char *b, char *x){if(x==NULL) fprintf(outfp,"\tMULA\t%s, %s\n",a,b);else fprintf(outfp,"\tMULA\t%s, %s, %s\n",a,b,x);}
+void MULL_ra(char *a, char *b, char *x){if(x==NULL) fprintf(outfp,"\tMULL\t%s, %s\n",a,b);else fprintf(outfp,"\tMULL\t%s, %s, %s\n",a,b,x);}
+void DIVA_ra(char *a, char *b, char *x){if(x==NULL) fprintf(outfp,"\tDIVA\t%s, %s\n",a,b);else fprintf(outfp,"\tDIVA\t%s, %s, %s\n",a,b,x);}
+void DIVL_ra(char *a, char *b, char *x){if(x==NULL) fprintf(outfp,"\tDIVL\t%s, %s\n",a,b);else fprintf(outfp,"\tDIVL\t%s, %s, %s\n",a,b,x);}
+
 void AND(char *a, char *b){fprintf(outfp,"\tAND\t%s, %s\n",a,b);}
 void OR(char *a, char *b){fprintf(outfp,"\tOR\t%s, %s\n",a,b);}
 void XOR(char *a, char *b){fprintf(outfp,"\tXOR\t%s, %s\n",a,b);}
+
+void AND_ra(char *a, char *b, char *x){if(x==NULL) fprintf(outfp,"\tAND\t%s, %s\n",a,b);else fprintf(outfp,"\tAND\t%s, %s, %s\n",a,b,x);}
+void OR_ra(char *a, char *b, char *x){if(x==NULL) fprintf(outfp,"\tOR\t%s, %s\n",a,b);else fprintf(outfp,"\tOR\t%s, %s, %s\n",a,b,x);}
+void XOR_ra(char *a, char *b, char *x){if(x==NULL) fprintf(outfp,"\tXOR\t%s, %s\n",a,b);else fprintf(outfp,"\tXOR\t%s, %s, %s\n",a,b,x);}
+
+void CPA(char *a, char *b){fprintf(outfp,"\tCPA\t%s, %s\n",a,b);}
+void CPL(char *a, char *b){fprintf(outfp,"\tCPA\t%s, %s\n",a,b);}
+
+void CPA_ra(char *a, char *b, char *x){if(x==NULL) fprintf(outfp,"\tCPA\t%s, %s\n",a,b);else fprintf(outfp,"\tCPA\t%s, %s, %s\n",a,b,x);}
+void CPL_ra(char *a, char *b, char *x){if(x==NULL) fprintf(outfp,"\tCPL\t%s, %s\n",a,b);else fprintf(outfp,"\tCPL\t%s, %s, %s\n",a,b,x);}
+
 
--- assignment4/src/mpplc.h
+++ assignment4/src/mpplc.h
@@ -4,6 +4,7 @@ void init_output(void);
 void end_output(void);
 
 void LD(char *a, char *b);
+void LD_ra(char *a, char *b, char *x);
 void ST(char *a, char *b);
 void LAD(char *a, char *b);
 
@@ -16,6 +17,25 @@ void MULL(char *a, char *b);
 void DIVA(char *a, char *b);
 void DIVL(char *a, char *b);
 
+void ADDA_ra(char *a, char *b, char *x);
+void ADDL_ra(char *a, char *b, char *x);
+void SUBA_ra(char *a, char *b, char *x);
+void SUBL_ra(char *a, char *b, char *x);
+void MULA_ra(char *a, char *b, char *x);
+void MULL_ra(char *a, char *b, char *x);
+void DIVA_ra(char *a, char *b, char *x);
+void DIVL_ra(char *a, char *b, char *x);
+
 void AND(char *a, char *b);
 void OR(char *a, char *b);
 void XOR(char *a, char *b);
+
+void AND_ra(char *a, char *b, char *x);
+void OR_ra(char *a, char *b, char *x);
+void XOR_ra(char *a, char *b, char *x);
+
+void CPA(char *a, char *b);
+void CPL(char *a, char *b);
+
+void CPA_ra(char *a, char *b, char *x);
+void CPL_ra(char *a, char *b, char *x);
